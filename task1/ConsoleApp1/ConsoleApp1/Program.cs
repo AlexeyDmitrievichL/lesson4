8176a7abe77d5d684632f3b1dcfb58fdca59c7ef
Alexey Lapushinsky <alexey.lapushinsky@solbeg.com>
2022-08-21 13:52:40 +0300

    task1 changes (lesson4)

--- task1/ConsoleApp1/ConsoleApp1/Program.cs
+++ task1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,8 +13,10 @@ namespace ConsoleApp1
             float arMean;
             GetArrayLength(out arrayLength);
             sbyte[] array = new sbyte[arrayLength];
-            FullArray(arrayLength, ref array, out max, out min, out sum, out arMean);
+            sbyte[] sortedArray = new sbyte[arrayLength];
+            FillArray(arrayLength, ref array, ref sortedArray, out max, out min, out sum, out arMean);
             Console.WriteLine($"Array: {String.Join(" ", array)}");
+            Console.WriteLine($"Sorted array: {String.Join(" ", sortedArray)}");
             Console.WriteLine($"Max: {max}");
             Console.WriteLine($"Min: {min}");
             Console.WriteLine($"Sum: {sum}");
@@ -22,16 +24,16 @@ namespace ConsoleApp1
             Console.ReadKey();
         }
 
-        private static int GetArrayLength(out int length)
+        private static void GetArrayLength(out int length)
         {
             Console.WriteLine("Set length of your array, please:");
             length = Convert.ToInt32(Console.ReadLine());
-            return length;
         }
 
-        private static void FullArray(
+        private static void FillArray(
             in int length,
             ref sbyte[] array,
+            ref sbyte[] sortedArray,
             out sbyte max,
             out sbyte min,
             out int sum,
@@ -46,12 +48,63 @@ namespace ConsoleApp1
             for (int i = 0; i < length; i++)
             {
                 sbyte nextValue = (sbyte)randm.Next(-100, 100);
-                calculateArrayData(i, nextValue, length, ref max, ref min, ref sum, ref arMean);
+                SortArray(i, nextValue, ref sortedArray);
+                calculateArrayInfo(i, nextValue, length, ref max, ref min, ref sum, ref arMean);
                 array[i] = nextValue;
             }
         }
 
-        private static void calculateArrayData(
+        private static void SortArray(in int iteration, in sbyte nextValue, ref sbyte[] sortedArray)
+        {
+            if (iteration == 0)
+            {
+                sortedArray[iteration] = nextValue;
+            }
+            else
+            {
+                sbyte[] updatedSortedArray = new sbyte[sortedArray.Length];
+                if (nextValue >= sortedArray[0])
+                {
+                    updatedSortedArray[0] = nextValue;
+                    for (int j = 1; j < updatedSortedArray.Length; j++)
+                    {
+                        updatedSortedArray[j] = sortedArray[j - 1];
+                    }
+                }
+                else if (nextValue <= sortedArray[iteration - 1])
+                {
+                    for (int j = 0; j < updatedSortedArray.Length; j++)
+                    {
+                        updatedSortedArray[j] = sortedArray[j];
+                    }
+                    updatedSortedArray[iteration] = nextValue;
+                }
+                else
+                {
+                    for (int j = 1; j < sortedArray.Length - 1; j++)
+                    {
+                        if (nextValue <= sortedArray[j - 1] && nextValue >= sortedArray[j])
+                        {
+                            for (int k = 0; k < j; k++)
+                            {
+                                updatedSortedArray[k] = sortedArray[k];
+                            }
+                            updatedSortedArray[j] = nextValue;
+                            for (int k = j + 1; k < sortedArray.Length; k++)
+                            {
+                                updatedSortedArray[k] = sortedArray[k - 1];
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < updatedSortedArray.Length; j++)
+                {
+                    sortedArray[j] = updatedSortedArray[j];
+                }
+            }
+        }
+
+        private static void calculateArrayInfo(
             in int iteration,
             in sbyte value,
             in int length,
