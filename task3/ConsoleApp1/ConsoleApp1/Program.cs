e9e56402466b3bda182a858fbb5202bbc30dc039
Alexey Lapushinsky <alexey.lapushinsky@solbeg.com>
2022-08-22 20:15:56 +0300

    Added task3 to lesson4

---
+++ task3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Dictionary<string, string> dictionary = new Dictionary<string, string>()
+            {
+                { "alphabet", "алфавит" },
+                { "bread", "хлеб" },
+                { "cat", "кот" },
+                { "dog", "собака" },
+                { "elephant", "слон" },
+                { "fire", "огонь" },
+                { "government", "правительство" },
+                { "helicopter", "вертолёт" },
+                { "icecream", "мороженое" },
+                { "joke", "шутка" },
+            };
+            Console.WriteLine("Input word you want to translate:");
+            string word = Console.ReadLine().ToLower();
+            if (dictionary.ContainsKey(word))
+            {
+                Console.WriteLine($"{dictionary[word]}");
+            }
+            else
+            {
+                Console.WriteLine("There is no this word in our dictionary. Sorry, bro :(");
+            }
+            Console.ReadKey();
+        }
+    }
+}
